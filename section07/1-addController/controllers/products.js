f896337bfc126bd38ec410198444f2ce4f8bdd53
Mohammad Shahrasbi <mohammad.shahrasbi@opusinspection.com>
2019-06-18 16:06:44 -0700

    start project and adding controller

---
+++ section07/1-addController/controllers/products.js
@@ -0,0 +1,32 @@
+
+const products = [];
+
+exports.getAddProduct = (req, res, next) => {
+    res.render('add-product', { 
+        pageTitle: 'Add Product', 
+        path: '/admin/add-product',
+        productCSS: true,
+        formsCSS: true,
+        activeAddProduct: true
+    });
+};
+
+
+exports.postAddProduct = (req, res, next) => {
+    console.log("[add-products.js] " + req.body.title);
+
+    products.push({ title: req.body.title });
+    res.redirect('/');
+};
+
+exports.getProducts = (req, res, next) => {
+
+    res.render('shop', {
+        prods: products, 
+        pageTitle: 'Shop', 
+        path: '/', 
+        hasProducts: products.length > 0,
+        activeShop: true,
+        productCSS: true
+    });
+}

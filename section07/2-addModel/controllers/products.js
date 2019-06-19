6083c4aafe95b3ac4e3f2a83dfaee6c2327e1104
Mohammad Shahrasbi <mohammad.shahrasbi@opusinspection.com>
2019-06-19 16:50:45 -0700

    MVC complete

---
+++ section07/2-addModel/controllers/products.js
@@ -0,0 +1,34 @@
+
+const Product = require('../models/product');
+
+exports.getAddProduct = (req, res, next) => {
+    res.render('add-product', { 
+        pageTitle: 'Add Product', 
+        path: '/admin/add-product',
+        productCSS: true,
+        formsCSS: true,
+        activeAddProduct: true
+    });
+};
+
+
+exports.postAddProduct = (req, res, next) => {
+    console.log("[add-products.js] " + req.body.title);
+
+    const product = new Product(req.body.title);
+    product.save();
+    res.redirect('/');
+};
+
+exports.getProducts = (req, res, next) => {
+    const products = Product.fetchAll();
+
+    res.render('shop', {
+        prods: products, 
+        pageTitle: 'Shop', 
+        path: '/', 
+        hasProducts: products.length > 0,
+        activeShop: true,
+        productCSS: true
+    });
+}
---
+++ section07/2-addModel/models/product.js
@@ -0,0 +1,16 @@
+
+const Products = [];
+
+module.exports = class Product {
+    constructor(title) {
+        this.title = title;
+    }
+
+    save() {
+        Products.push(this);
+    }
+
+    static fetchAll() {
+        return Products;
+    }
+}

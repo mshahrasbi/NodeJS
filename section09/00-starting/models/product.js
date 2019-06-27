58d621c13e44a24cbf8b317fd56af138de7640fe
Mohammad Shahrasbi <mohammad.shahrasbi@opusinspection.com>
2019-06-26 17:59:03 -0700

    Update Shop

---
+++ section09/00-starting/models/product.js
@@ -0,0 +1,49 @@
+
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
+
+const getProductsFromFile = (callback) => {
+
+    fs.readFile(p, (err, fileContent) => {
+
+        if (err) {
+            return callback([]);
+        }
+
+        let products = JSON.parse(fileContent);
+        callback(products);
+    });
+}
+
+module.exports = class Product {
+    constructor(title, imageUrl, description, price) {
+        this.title = title;
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.price = price;
+    }
+
+    save() {
+        this.id = Math.random().toString();
+        getProductsFromFile( products => {
+            products.push(this);
+
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err);
+            });
+        });
+    }
+
+    static fetchAll(callback) {
+        getProductsFromFile(callback);
+    }
+
+    static findById(id, callback) {
+        getProductsFromFile(products => {
+            const product = products.find(p => p.id === id);
+            callback(product);
+        });
+    }
+}

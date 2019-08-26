70a3b9241c7ad7b2d6941db02533d1f9328148cd
Mohammad Shahrasbi <mohammad.shahrasbi@opusinspection.com>
2019-08-26 13:35:07 -0700

    remove pwd

--- section12/02-creating-products/util/database.js
+++ section12/02-creating-products/util/database.js
@@ -7,7 +7,7 @@ let _db;
 const mongoConnect = (cb) => {
     // at the beginning if database 'shop' is not exist, it will create it for us
     MongoClient
-    .connect('mongodb+srv://mshahrasbi:Majeedsh100@mycluster-l8bwl.mongodb.net/shop?retryWrites=true&w=majority')
+    .connect('mongodb+srv://mshahrasbi:???????@mycluster-l8bwl.mongodb.net/shop?retryWrites=true&w=majority')
     .then( client => {
         console.log('Connected!')
         _db = client.db();
